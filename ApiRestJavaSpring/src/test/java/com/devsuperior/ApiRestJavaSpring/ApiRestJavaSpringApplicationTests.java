6102b948493925104674becde8b82d823a69f387
Virgílio Pires da Costa <virgilio.pires.costa@gmail.com>
2023-04-30 21:28:26 -0300

    "add project API Rest Java Spring"

---
+++ ApiRestJavaSpring/src/test/java/com/devsuperior/ApiRestJavaSpring/ApiRestJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ApiRestJavaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
