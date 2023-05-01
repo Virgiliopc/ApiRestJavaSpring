572b73a22b5372dc1518bf2d05dfdb503516e4db
Virgílio Pires da Costa <virgilio.pires.costa@gmail.com>
2023-04-30 21:28:26 -0300

    "add project API Rest Java Spring"

---
+++ ApiRestJavaSpring/src/main/java/com/devsuperior/ApiRestJavaSpring/ApiRestJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ApiRestJavaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestJavaSpringApplication.class, args);
+	}
+
+}
